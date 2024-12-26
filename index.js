3e0afbff9b5f274ab3559d08873eda55a1b6275b
Marcos Daniel da Silva Lima <marcos.d.s.lima1408@gmail.com>
2024-12-26 14:34:18 -0300

    commit 11.3

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.post('/webhook', (req, res) => {
         return;
       }
       // Senão, retorna uma mensagem de sucesso
-      res.json({ fulfillmentText: 'Valor inserido com sucesso ${userNome}!'});
+      res.json({ fulfillmentText: `Valor inserido com sucesso ${userNome}!`});
     });
     // Caso ele não tenha encontrado a intent especificada, ele retorna uma mensagem de erro
   } else {
